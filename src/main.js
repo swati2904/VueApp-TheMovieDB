afb096ba9e3c189c893d705ee3e9f91aa8b3ae06
swati2904 <swati.lpu17@gmail.com>
2020-04-20 00:46:03 +0530

    vue-CLI template created for base setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Imported the Vue's All Ant design component
+import Antd from 'ant-design-vue';
+
+// CSS separatel needs to import
+import 'ant-design-vue/dist/antd.css';
+
+// Need to tell vue js to use AntDesign
+Vue.use(Antd);
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
